e4ba626757c2a1b9ca47f189451c7e06ec7c1b84
BIRUK TESFAYE <biruktesfayeve@gmail.com>
2024-03-21 07:40:12 +0300

    Update footer component layout and content

--- src/lib/components/_shared/footer/footer.svelte
+++ src/lib/components/_shared/footer/footer.svelte
@@ -1,64 +1,202 @@
-<footer class=" bg-black text-white py-8 px-6">
-	<div class="max-w-7xl mx-auto">
-		<div class="flex flex-row gap-12 mb-8">
-			<!-- Logo, address, and contact information -->
-			<!-- Links -->
-			<div class="flex flex-1 justify-between gap-8">
-				<div class="">
-					<div>
-						<img src="logo.png" alt="Logo" class="mb-4" />
-						<p>123 Main St, City, Country</p>
-						<p>Email: info@example.com</p>
-						<p>Phone: +1 (123) 456-7890</p>
+<div class=" bg-[#252525] text-white">
+	<div class="max-w-7xl mx-auto py-10 px-4 lg:px-16">
+		<div class="w-full flex justify-between text-left flex-col lg:flex-row">
+			<div class="flex-1 pr-[10%]">
+				<img
+					width="220"
+					class="mb-4"
+					src="https://igad.int/wp-content/uploads/2021/07/White-Logo.svg"
+					alt="IGAD Logo"
+				/>
+				<div>
+					<p class="pb-5 text-sm">
+						IGAD Secretariat Avenue Georges Clemenceau,
+						<br />
+						P.O. Box 2653 Djibouti,
+						<br />
+						Republic of Djibouti
+					</p>
+					<p class="pb-5 text-sm">
+						+253-21354050
+						<br />
+						+253-21356994<br />
+						<a
+							class="text-[#AEB6C7] leading-9 text-xs"
+							href="https://igad.int/privacy-policy/"
+							target="_blank"
+							rel="noopener">Privacy Policy |</a
+						>
+						<a
+							class="text-[#AEB6C7] leading-9 text-xs"
+							href="https://igad.int/cookie-policy/"
+							target="_blank"
+							rel="noopener"
+						>
+							Cookie Policy</a
+						>
+					</p>
+				</div>
+			</div>
+			<div class="flex-grow-[1] flex-shrink-[1] text-left">
+				<div class="w-full flex flex-col justify-between h-full flex-wrap">
+					<div class="flex flex-wrap justify-between text-left mb-3">
+						<div class="flex flex-col">
+							<h6 class="text-bold">About IGAD</h6>
+							<a
+								class="text-[#AEB6C7] leading-9 text-xs"
+								href="https://igad.int/about/?tab=the-history"
+							>
+								The History
+							</a>
+							<a
+								class="text-[#AEB6C7] leading-9 text-xs"
+								href="https://igad.int/about/?tab=aims-and-objectives"
+							>
+								Aims and Objectives
+							</a>
+							<a
+								class="text-[#AEB6C7] leading-9 text-xs"
+								href="https://igad.int/about/?tab=igad-structure"
+							>
+								IGAD Structure
+							</a>
+							<a
+								class="text-[#AEB6C7] leading-9 text-xs"
+								href="https://igad.int/about/?tab=the-igad-region"
+							>
+								The IGAD Region
+							</a>
+
+							<a
+								class="text-[#AEB6C7] leading-9 text-xs"
+								href="https://igad.int/about/?tab=our-strategy"
+							>
+								Our Strategy
+							</a>
+							<a
+								class="text-[#AEB6C7] leading-9 text-xs"
+								href="https://igad.int/about/?tab=the-executive-secretary"
+							>
+								The Executive Secretary
+							</a>
+						</div>
+						<div class="flex flex-col">
+							<h6 class="text-bold">Divisions</h6>
+							<a
+								class="text-[#AEB6C7] leading-9 text-xs"
+								href="https://igad.int/agriculture-environment/"
+							>
+								Agriculture and Environment
+							</a>
+							<a class="text-[#AEB6C7] leading-9 text-xs" href="https://igad.int/peace-security/">
+								Peace and Security
+							</a>
+							<a
+								class="text-[#AEB6C7] leading-9 text-xs"
+								href="https://igad.int/economic-cooperation/"
+							>
+								Economic Cooperation &amp; Regional Integration
+							</a>
+							<a
+								class="text-[#AEB6C7] leading-9 text-xs"
+								href="https://igad.int/health-social-development/"
+							>
+								Health and Social Development
+							</a>
+							<a
+								class="text-[#AEB6C7] leading-9 text-xs"
+								href="https://igad.int/igad-whistleblowing/"
+							>
+								IGAD Whistleblower Form
+							</a>
+						</div>
+						<div class="flex flex-col">
+							<h6 class="text-bold">Resources</h6>
+							<a
+								class="text-[#AEB6C7] leading-9 text-xs"
+								href="https://outlook.office365.com/"
+								target="_blank"
+								rel="noopener"
+							>
+								Webmail
+							</a>
+							<a class="text-[#AEB6C7] leading-9 text-xs" href="https://igad.int/news-archive/">
+								News and Updates
+							</a>
+							<a class="text-[#AEB6C7] leading-9 text-xs" href="https://igad.int/events-calendar/">
+								Upcoming Events
+							</a>
+							<a
+								class="text-[#AEB6C7] leading-9 text-xs"
+								href="https://igad.int/category/magazines-and-reports/"
+							>
+								Magazines and Reports
+							</a>
+							<a class="text-[#AEB6C7] leading-9 text-xs" href="https://igad.int/fraud-fake-news//">
+								Fraud and Fake News
+							</a>
+							<a
+								class="text-[#AEB6C7] leading-9 text-xs"
+								href="https://flic.kr/ps/3WNfVS"
+								target="_blank"
+								rel="noopener"
+							>
+								Gallery
+							</a>
+							<h6 class="text-bold">Tools</h6>
+							<a class="text-[#AEB6C7] leading-9 text-xs" href="https://igad.int">
+								All IGAD Websites
+							</a>
+							<a class="text-[#AEB6C7] leading-9 text-xs" href="https://eahazardswatch.icpac.net/">
+								East Africa Hazards Watch
+							</a>
+							<a class="text-[#AEB6C7] leading-9 text-xs" href="https://3w.igad.int/map/">
+								IGAD 3W Maps
+							</a>
+							<a class="text-[#AEB6C7] leading-9 text-xs" href="https://geonode.igad.int/">
+								IGAD Maps GEONODE
+							</a>
+						</div>
+						<div class="flex flex-col">
+							<h6 class="text-bold">Opportunities</h6>
+							<a class="text-[#AEB6C7] leading-9 text-xs" href="https://igad.int/careers/">
+								Vacancies
+							</a>
+							<a class="text-[#AEB6C7] leading-9 text-xs" href="https://igad.int/tenders/">
+								Tenders
+							</a>
+							<a class="text-[#AEB6C7] leading-9 text-xs" href="https://igad.int/consultancies/">
+								Consultancies
+							</a>
+							<a class="text-[#AEB6C7] leading-9 text-xs" href="https://igad.int/contracts/">
+								Contracts
+							</a>
+						</div>
+					</div>
+					<div class="flex justify-between">
+						<div>
+							<p class="text-xs">© 2024 Copyright. IGAD. All rights reserved.</p>
+						</div>
+						<div>
+							<a href="https://www.facebook.com/igadsecretariat/" target="_blank" rel="noopener"
+								><i class="fab fa-facebook"></i></a
+							>
+							<a href="https://twitter.com/igadsecretariat/" target="_blank" rel="noopener"
+								><i class="fab fa-twitter"></i></a
+							>
+							<a
+								href="https://www.youtube.com/channel/UCDgq86NAvL74tvpQx614qig/"
+								target="_blank"
+								rel="noopener"><i class="fab fa-youtube"></i></a
+							>
+
+							<a href="https://www.instagram.com/igad_secretariat/" target="_blank" rel="noopener"
+								><i class="fab fa-instagram"></i></a
+							>
+						</div>
 					</div>
 				</div>
-				<div>
-					<h3 class="font-bold mb-2">About IGAD</h3>
-					<ul class="space-y-2">
-						<li><a href="#">Link 1</a></li>
-						<li><a href="#">Link 2</a></li>
-						<!-- More links -->
-					</ul>
-				</div>
-
-				<div>
-					<h3 class="font-bold mb-2">Divisions</h3>
-					<ul class="space-y-2">
-						<li><a href="#">Link 1</a></li>
-						<li><a href="#">Link 2</a></li>
-						<!-- More links -->
-					</ul>
-				</div>
-
-				<div>
-					<h3 class="font-bold mb-2">Resources</h3>
-					<ul class="space-y-2">
-						<li><a href="#">Link 1</a></li>
-						<li><a href="#">Link 2</a></li>
-						<!-- More links -->
-					</ul>
-				</div>
-
-				<div>
-					<h3 class="font-bold mb-2">Opportunities</h3>
-					<ul class="space-y-2">
-						<li><a href="#">Link 1</a></li>
-						<li><a href="#">Link 2</a></li>
-						<!-- More links -->
-					</ul>
-				</div>
 			</div>
-		</div>
-
-		<!-- Copyright and social links -->
-		<div class="flex justify-between items-center">
-			<div>
-				<a href="#"><img src="facebook.png" alt="Facebook" class="inline-block mr-2" /></a>
-				<a href="#"><img src="twitter.png" alt="Twitter" class="inline-block mr-2" /></a>
-				<!-- More social links -->
-			</div>
-
-			<p class="text-center">&copy; 2022 IGAD. All rights reserved.</p>
 		</div>
 	</div>
-</footer>
+</div>
