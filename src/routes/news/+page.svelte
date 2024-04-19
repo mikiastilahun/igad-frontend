3bec83a3139f9a58ae19727c2c22f3b478de0388
BIRUK TESFAYE <biruktesfayeve@gmail.com>
2024-04-19 14:15:30 +0300

    Refactor styling in +page.svelte to improve responsiveness

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -8,9 +8,11 @@
 	export let data;
 
 	const news = data.data?.newsData.data;
+
+	$: console.log({ data });
 </script>
 
-<div class="p-4">
+<div class="md:p-4">
 	<section class="relative min-h-[674px] flex items-center rounded-lg">
 		<img
 			class="absolute inset-0 object-cover w-full h-full rounded-lg"
@@ -20,7 +22,7 @@
 		<div
 			class="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black/80 to-transparent rounded-lg"
 		></div>
-		<div class="relative max-w-[1136px] mx-auto w-full">
+		<div class="relative max-w-[1136px] mx-auto w-full px-4 md:px-8">
 			<div class="text-white max-w-2xl">
 				<h1 class="text-3xl font-bold mb-2">News</h1>
 				<p class=" text-base font-normal leading-normal">
@@ -33,11 +35,11 @@
 	</section>
 </div>
 
-<section class="max-w-[1136px] mx-auto py-10">
-	<div class=" flex items-start gap-16 w-full">
+<section class="max-w-[1136px] mx-auto py-10 px-8 md:px-4">
+	<div class=" flex flex-col-reverse lg:flex-row flex-wrap gap-16 w-full items-stretch">
 		<!-- news -->
 		<div class="flex flex-col gap-5 justify-center items-start">
-			<h2 class="text-2xl font-bold">Latest News</h2>
+			<h2 class="text-2xl hidden lg:inline-block font-bold">Latest News</h2>
 			<!-- news card -->
 			<div class="flex flex-col gap-5">
 				{#if news?.length !== 0}
@@ -57,23 +59,26 @@
 		<div class="relative flex-1 hover:cursor-pointer">
 			<div class="absolute top-0 left-0 h-full w-full bg-black opacity-60 rounded-md"></div>
 			<enhanced:img
-				class="object-cover rounded-md"
+				class="object-cover rounded-md h-full"
 				src={news?.[0].attributes.thumbnail.data.attributes.url
 					? `${PUBLIC_STATIC_URL}${news?.[0].attributes.thumbnail.data.attributes.url}`
 					: NewsImg2}
 				alt="alt text"
 			/>
-			<div class="absolute bottom-0 px-8 pb-8 text-white">
+			<div class="absolute bottom-0 px-4 md:px-8 pb-4 md:pb-8 text-white">
 				<span class="text-base leading-normal">Debits - 03 June, 2023</span>
-				<h3 class="text-3xl font-bold leading-normal">{news?.[0].attributes.title}</h3>
+				<h3 class="text-lg sm:text-xl md:text-3xl font-semibold md:font-bold md:leading-normal">
+					{news?.[0].attributes.title}
+				</h3>
 			</div>
 		</div>
+		<h2 class="text-2xl lg:hidden inline-block font-bold">Latest News</h2>
 	</div>
 </section>
 
 <!-- List of news -->
 
-<section class="max-w-[1136px] mx-auto py-10">
+<section class="max-w-[1136px] mx-auto py-10 px-8 md:px-4">
 	<h2 class="text-2xl font-bold">Our News</h2>
 	<div class="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
 		<Card />
@@ -89,14 +94,16 @@
 <!-- twitter will be here -->
 
 <!-- email subscription -->
-<section class="max-w-[1136px] h-[384px] mx-auto my-10">
-	<div class="h-full flex">
-		<div class="w-[410px] rounded-lg bg-primary flex justify-center items-center px-10">
+<section class="max-w-[1136px] md:h-[384px] mx-auto my-10">
+	<div class="h-full flex flex-col md:flex-row">
+		<div
+			class="py-28 md:w-[410px] rounded-b-none md:rounded-b-lg rounded-lg bg-primary flex justify-center items-center px-10"
+		>
 			<h3 class="text-white font-bold text-[26px]">
 				Subscribe to our newsletter to stay up-to date.
 			</h3>
 		</div>
-		<form class="px-28 flex flex-col gap-8 justify-center flex-1">
+		<form class="py-10 px-8 md:px-4 lg:px-28 flex flex-col gap-8 justify-center flex-1">
 			<label for="email" class="flex flex-col gap-2">
 				<span class="text-base text-gray-700">Email</span>
 				<input
--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -21,7 +21,7 @@
 		<div
 			class="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black/80 to-transparent rounded-lg"
 		></div>
-		<div class="relative max-w-[1136px] mx-auto w-full px-4 md:px-2">
+		<div class="relative max-w-[1136px] mx-auto w-full px-4 md:px-8">
 			<div class="text-white max-w-2xl">
 				<h1 class="text-3xl font-bold mb-2">Reports and Publications</h1>
 				<p class=" text-base font-normal leading-normal">
@@ -35,7 +35,7 @@
 </div>
 
 <!-- Featured -->
-<section class="max-w-[1136px] mx-auto py-10 flex flex-col gap-8 px-4 sm:px-8 md:px-2">
+<section class="max-w-[1136px] mx-auto py-10 flex flex-col gap-8 px-4 sm:px-8 md:px-4">
 	<h2 class="text-2xl font-bold leading-normal">Featured</h2>
 	<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
 		<!-- cards -->
@@ -47,7 +47,7 @@
 </section>
 
 <!-- Full width search bar -->
-<section class=" py-10">
+<section class=" py-10 px-4">
 	<div class="max-w-[1136px] mx-auto">
 		<div class="relative flex items-center justify-center">
 			<input
@@ -63,7 +63,7 @@
 </section>
 
 <!-- All Publications -->
-<section class="max-w-[1136px] mx-auto py-10 flex flex-col gap-8 px-4 md:px-2">
+<section class="max-w-[1136px] mx-auto py-10 flex flex-col gap-8 px-4">
 	<h2 class="text-2xl font-bold leading-normal">All Publications</h2>
 	<div class="flex gap-8 p-4 flex-col md:flex-row">
 		<!-- filter -->
@@ -115,7 +115,7 @@
 			{#each Array(4) as _}
 				<!-- cards -->
 				<div class="flex flex-col lg:flex-row gap-4 md:gap-5 rounded-lg">
-					<div class="w-full lg:w-[245px] h-[200px] lg:h-auto flex-none rounded-lg">
+					<div class="w-full lg:w-[245px] h-[200px] lg:h-[150px] flex-none rounded-lg">
 						<img
 							height="150px"
 							class="w-full flex-1 h-full object-contain bg-primary rounded-b-none rounded-lg md:rounded-b-lg"
@@ -133,10 +133,10 @@
 								<div class="text-white text-[10px] font-normal font-['Open Sans']">Report</div>
 							</div>
 						</div>
-						<h2 class="text-lg leading-6 font-bold">
+						<h2 class="text-lg leading-6 font-bold line-clamp-1">
 							Lorem ipsum dolor sit amet consectetur adipisicing elit.
 						</h2>
-						<p class=" text-zinc-500 text-sm">
+						<p class=" text-zinc-500 text-sm line-clamp-5">
 							The eight countries under the Intergovernmental Authority on Development (IGAD) of
 							Eastern Africa considered countries in the African continent that are vulnerable to
 							climate change and Extreme events(IPCC, 2018). Change and variability in extreme
@@ -147,12 +147,12 @@
 				</div>
 			{/each}
 
-			<div class="justify-center items-center gap-4 inline-flex pt-10">
+			<div class="justify-center items-center gap-1 md:gap-4 inline-flex pt-10">
 				<!-- pagination 20+12-->
 				<button
 					class=" px-4 py-2 bg-white rounded-lg border border-neutral-900 border-opacity-10 justify-center items-center gap-1 inline-flex"
 				>
-					<ChevronDown class=" " />
+					<ChevronDown class=" fill-primary" />
 				</button>
 				{#each Array(5) as _, index}
 					<button
@@ -164,7 +164,7 @@
 				<button
 					class=" px-4 py-2 bg-white rounded-lg border border-neutral-900 border-opacity-10 justify-center items-center gap-1 inline-flex"
 				>
-					<ChevronDown class="rotate-180" />
+					<ChevronDown class="rotate-180 fill-primary" />
 				</button>
 			</div>
 		</div>
