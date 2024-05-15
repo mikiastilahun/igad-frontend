d2fee2f16e6c826ea6efa0395657ec644a8d5367
BIRUK TESFAYE <biruktesfayeve@gmail.com>
2024-05-15 20:36:43 +0300

    feat: Add tailwind typography plugin

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,105 @@
+<script>
+	import Hero from '$lib/assets/publications/hero.jpg';
+	import SvelteMarkdown from 'svelte-markdown';
+
+	// define the content with sample markdown text and images included
+	const content = `
+# Markdown syntax guide
+
+## Headers
+
+# This is a Heading h1
+## This is a Heading h2
+###### This is a Heading h6
+
+## Emphasis
+
+*This text will be italic*  
+_This will also be italic_
+
+**This text will be bold**  
+__This will also be bold__
+
+_You **can** combine them_
+
+## Lists
+
+### Unordered
+
+* Item 1
+* Item 2
+* Item 2a
+* Item 2b
+
+### Ordered
+
+1. Item 1
+2. Item 2
+3. Item 3
+    1. Item 3a
+    2. Item 3b
+
+## Images
+
+![This is an alt text.](/image/sample.webp "This is a sample image.")
+
+## Links
+
+You may be using [Markdown Live Preview](https://markdownlivepreview.com/).
+
+## Blockquotes
+
+> Markdown is a lightweight markup language with plain-text-formatting syntax, created in 2004 by John Gruber with Aaron Swartz.
+>
+>> Markdown is often used to format readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.
+
+## Tables
+
+| Left columns  | Right columns |
+| ------------- |:-------------:|
+| left foo      | right foo     |
+| left bar      | right bar     |
+| left baz      | right baz     |
+
+## Blocks of code
+
+\`\`\`
+let message = 'Hello world';
+alert(message);
+\`\`\`
+
+## Inline code
+
+This web site is using \`markedjs/marked\`.
+`;
+</script>
+
+<div class="md:p-4">
+	<section class="relative min-h-[674px] flex items-center rounded-lg">
+		<img
+			class="absolute inset-0 object-cover w-full h-full rounded-lg"
+			alt={`publications hero image`}
+			src={Hero}
+		/>
+		<div
+			class="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black/80 to-transparent rounded-lg"
+		></div>
+		<div class="relative max-w-[1136px] mx-auto w-full px-4 md:px-8">
+			<div class="text-white max-w-2xl">
+				<h1 class="text-3xl font-bold mb-2">Projects</h1>
+				<p class=" text-base font-normal leading-normal">
+					Lorem ipsum dolor sit amet consectetur. Egestas nulla ullamcorper pretium sit nibh sapien
+					vel phasellus eu. Aliquet facilisis enim dui ridiculus. Sit ipsum sollicitudin sapien
+					aliquam. Sodales pulvinar facilisi donec facilisis
+				</p>
+			</div>
+		</div>
+	</section>
+</div>
+
+<div class="w-full">
+	<!-- content -->
+	<p class=" prose max-w-5xl mx-auto">
+		<SvelteMarkdown source={content} />
+	</p>
+</div>
