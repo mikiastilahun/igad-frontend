fd1f6e7ad494cdddab58ded9df477e321bfd74ac
BIRUK TESFAYE <biruktesfayeve@gmail.com>
2024-05-23 09:48:11 +0300

    feat: Fetch population statistics from public API in +page.server.ts

--- src/routes/statistics/+page.server.ts
+++ src/routes/statistics/+page.server.ts
@@ -2,6 +2,88 @@ import type { Load } from '@sveltejs/kit';
 import { PUBLIC_STRAPI_URL } from '$env/static/public';
 import type { PopulationStats } from '../+page.server.js';
 
+
+export type AgeGroup = 'age 0-4' |
+	'age 5-9' |
+	'age 10-14' |
+	'age 15-19' |
+	'age 20-24' |
+	'age 25-29' |
+	'age 30-34' |
+	'age 35-39' |
+	'age 40-44' |
+	'age 45-49' |
+	'age 50-54' |
+	'age 55-59' |
+	'age 60-64' |
+	'age 65-69' |
+	'age 70-74' |
+	'age 75-79' |
+	'age 80+';
+export type PopulationPerCountryStats = {
+	data: {
+		id: number;
+		attributes: {
+			Ethiopia: {
+				year: string;
+				male: number;
+				female: number;
+				age_group: AgeGroup;
+			}[],
+			Kenya: {
+				year: string;
+				male: number;
+				female: number;
+				age_group: AgeGroup;
+			}[],
+			SouthSudan: {
+				year: string;
+				male: number;
+				female: number;
+				age_group: AgeGroup;
+			}[],
+			Eritrea: {
+				year: string;
+				male: number;
+				female: number;
+				age_group: AgeGroup;
+			}[],
+			Somalia: {
+				year: string;
+				male: number;
+				female: number;
+				age_group: AgeGroup;
+			}[],
+			Uganda: {
+				year: string;
+				male: number;
+				female: number;
+				age_group: AgeGroup;
+			}[],
+			Sudan: {
+				year: string;
+				male: number;
+				female: number;
+				age_group: AgeGroup;
+			}[],
+			Djibouti: {
+				year: string;
+				male: number;
+				female: number;
+				age_group: AgeGroup;
+			}[],
+			createdAt: string;
+			updatedAt: string;
+			publishedAt: string;
+		};
+	};
+	meta: any;
+}
+
+type ApiResponseStats = {
+	populationStats: PopulationStats;
+	populationPerCountry: PopulationPerCountryStats;
+}
 export const load: Load = async ({ fetch }) => {
 	process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 	try {
@@ -11,11 +93,21 @@ export const load: Load = async ({ fetch }) => {
 			throw new Error(`HTTP error! status: ${populationStatsResponse.status}`);
 		}
 
-		const populationStats = await populationStatsResponse.json();
+		const populationPerCountryStatsResponse = await fetch(`${PUBLIC_STRAPI_URL}/api/population-per-country?populate=*`);
+
+		if (!populationPerCountryStatsResponse.ok) {
+			throw new Error(`HTTP error! status: ${populationStatsResponse.status}`);
+		}
+
+
+		const data: ApiResponseStats = {
+			populationStats: await populationStatsResponse.json(),
+			populationPerCountry: await populationPerCountryStatsResponse.json(),
+		};
 
 
 
-		return populationStats as PopulationStats;
+		return { data }
 	} catch (e: unknown) {
 		console.log({ e });
 		return { error: (e as Error).message };
