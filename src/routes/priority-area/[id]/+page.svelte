0da689f1e373aa72253224ab3c960090fa542214
BIRUK TESFAYE <biruktesfayeve@gmail.com>
2024-06-05 16:02:09 +0300

    refactor: Update priority area pages

--- src/routes/priority-area/[id]/+page.svelte
+++ src/routes/priority-area/[id]/+page.svelte
@@ -10,10 +10,10 @@
 </script>
 
 <div class="md:p-4">
-	<section class="relative min-h-[674px] flex items-center rounded-lg bg-primary">
-		<div class="max-w-7xl mx-auto flex items-center h-full gap-28">
+	<section class="relative flex min-h-[674px] items-center rounded-lg bg-primary">
+		<div class="mx-auto flex h-full max-w-7xl items-center gap-28">
 			<div class="max-w-3xl">
-				<h1 class="text-xl font-bold mb-2 text-white">{priorityArea?.attributes.Title}</h1>
+				<h1 class="mb-2 text-xl font-bold text-white">{priorityArea?.attributes.Title}</h1>
 				<p class="text-white">{priorityArea?.attributes.ShortDescription}</p>
 			</div>
 
@@ -27,9 +27,9 @@
 	</section>
 </div>
 
-<section class="py-10 pl-8 flex gap-8 max-w-[1136px] mx-auto items-start relative">
-	<div class="w-full max-w-5xl ml-auto">
-		<h2 class="text-3xl font-bold text-primary mt-4 mb-4 pb-2" id="header">
+<section class="relative mx-auto flex max-w-[1136px] items-start gap-8 py-10 pl-8">
+	<div class="ml-auto w-full max-w-5xl">
+		<h2 class="mb-4 mt-4 pb-2 text-3xl font-bold text-primary" id="header">
 			{priorityArea?.attributes.header}
 		</h2>
 		<!-- content -->
@@ -39,17 +39,17 @@
 
 		<!-- IGAD ACTIONS -->
 		<div class="mt-10">
-			<h1 class="text-2xl font-bold mb-4">Actions for IGAD</h1>
+			<h1 class="mb-4 text-2xl font-bold">Actions for IGAD</h1>
 
 			<div class="flex flex-col">
 				{#each priorityArea?.attributes.IgadActions || [] as action}
-					<h2 class="text-primary text-lg font-bold" id={action.ActionTitle}>
+					<h2 class="text-lg font-bold text-primary" id={action.ActionTitle}>
 						{action.ActionTitle}
 					</h2>
 					<!-- ul with action.actionItemList -->
-					<ul class=" border-gray-300 rounded-md p-4 mb-4">
+					<ul class=" mb-4 rounded-md border-gray-300 p-4">
 						{#each action.actionItemList || [] as item}
-							<li class="gap-2 list-disc">
+							<li class="list-disc gap-2">
 								{item.item}
 							</li>
 						{/each}
@@ -59,17 +59,17 @@
 		</div>
 		<!-- MemberState ACTIONS -->
 		<div class="mt-10">
-			<h1 class="text-3xl font-bold mb-4">Actions for Member States</h1>
+			<h1 class="mb-4 text-3xl font-bold">Actions for Member States</h1>
 
 			<div class="flex flex-col">
 				{#each priorityArea?.attributes.memberStateActions || [] as action}
-					<h2 class="text-primary text-lg font-bold" id={action.ActionTitle}>
+					<h2 class="text-lg font-bold text-primary" id={action.ActionTitle}>
 						{action.ActionTitle}
 					</h2>
 					<!-- ul with action.actionItemList -->
-					<ul class=" border-gray-300 rounded-md p-4 mb-4">
+					<ul class=" mb-4 rounded-md border-gray-300 p-4">
 						{#each action.actionItemList || [] as item}
-							<li class="gap-2 list-disc">
+							<li class="list-disc gap-2">
 								{item.item}
 							</li>
 						{/each}
@@ -82,11 +82,11 @@
 			<h1 class="text-2xl font-bold" id="expected_outcome">Expected Outcomes</h1>
 
 			<div class="flex flex-col">
-				<ul class=" border-gray-300 rounded-md p-4 mb-4 list-disc list-outside">
+				<ul class=" mb-4 list-outside list-disc rounded-md border-gray-300 p-4">
 					{#each priorityArea?.attributes.expectedOutcomes || [] as action}
 						<!-- ul with action.actionItemList -->
 
-						<li class="gap-2 list-disc">
+						<li class="list-disc gap-2">
 							{action.item}
 						</li>
 					{/each}
@@ -95,45 +95,41 @@
 		</div>
 
 		<!-- Stackholders and partners -->
-		<div class="bg-secondary p-6 mb-4 rounded-md flex flex-col gap-4">
-			<div>
-				<h1 class="font-bold">
-					<span class="flex gap-2 items-center"> <Stackholders />Stackholders</span>
+		<div class="mb-4 flex flex-col gap-8 rounded-md bg-secondary p-6">
+			<div class="grid gap-3">
+				<h1 class="text-lg font-bold">
+					<span class="flex items-center gap-2"> <Stackholders />Stackholders</span>
 				</h1>
-				<div class="flex gap-2 font-light ml-7">
+				<div class="ml-7 flex flex-wrap gap-2">
 					{#each priorityArea?.attributes.stackholders || [] as stackholder}
 						<a
 							href={stackholder.url}
 							class="
-						hover:text-primary transition-colors duration-300 ease-in-out
+							text-nowrap text-stone-900 transition-colors duration-300 ease-in-out hover:text-primary
 						">{stackholder.label}</a
 						>
-						<span>
-							{#if stackholder !== priorityArea?.attributes.stackholders[priorityArea?.attributes.stackholders.length - 1]}
-								,
-							{/if}
-						</span>
+						{#if stackholder !== priorityArea?.attributes.stackholders[priorityArea?.attributes.stackholders.length - 1]}
+							<div class="font-['Open Sans'] text-base font-normal text-stone-900">•</div>
+						{/if}
 					{/each}
 				</div>
 			</div>
 
-			<div>
-				<h1 class="font-bold">
-					<span class="flex gap-2 items-center"> <Stackholders />Partners</span>
+			<div class="grid gap-3">
+				<h1 class="text-lg font-bold">
+					<span class="flex items-center gap-2"> <Stackholders />Partners</span>
 				</h1>
-				<div class="flex gap-2 font-light ml-7">
+				<div class="ml-7 flex flex-wrap gap-2">
 					{#each priorityArea?.attributes.partners || [] as partner}
 						<a
 							href={partner.url}
 							class="
-						hover:text-primary transition-colors duration-300 ease-in-out
+							text-nowrap text-stone-900 transition-colors duration-300 ease-in-out hover:text-primary
 						">{partner.label}</a
 						>
-						<span>
-							{#if partner !== priorityArea?.attributes.partners[priorityArea?.attributes.partners.length - 1]}
-								,
-							{/if}
-						</span>
+						{#if partner !== priorityArea?.attributes.partners[priorityArea?.attributes.partners.length - 1]}
+							<div class="font-['Open Sans'] text-base font-normal text-stone-900">•</div>
+						{/if}
 					{/each}
 				</div>
 			</div>
@@ -141,22 +137,22 @@
 	</div>
 
 	<!-- side nav with header selector-->
-	<div class="w-full max-w-72 sticky top-10">
+	<div class="sticky top-10 w-full max-w-72">
 		<div class="flex flex-col">
 			<a
 				href="#header"
-				class="border-l-2 hover:border-l-primary-600 px-3 transition-colors duration-300 ease-in-out text-stone-900 hover:text-primary"
+				class="border-l-2 px-3 text-stone-900 transition-colors duration-300 ease-in-out hover:border-l-primary-600 hover:text-primary"
 				>{priorityArea?.attributes.header}</a
 			>
 
-			<div class="py-4 border-l-2">
-				<p class="px-3 pb-2 text-green-600 text-sm font-semibold">Action for IGAD</p>
+			<div class="border-l-2 py-4">
+				<p class="px-3 pb-2 text-sm font-semibold text-green-600">Action for IGAD</p>
 				<ul class=" flex flex-col gap-2">
 					{#each priorityArea?.attributes.IgadActions || [] as action}
-						<li class="px-6 list-none border-l-2 hover:border-l-primary-600 -m-[2px]">
+						<li class="-m-[2px] list-none border-l-2 px-6 hover:border-l-primary-600">
 							<a
 								href="#{action.ActionTitle}"
-								class="hover:text-primary text-stone-900 text-sm transition-colors duration-300 ease-in-out"
+								class="text-sm text-stone-900 transition-colors duration-300 ease-in-out hover:text-primary"
 								>{action.ActionTitle}</a
 							>
 						</li>
@@ -165,13 +161,13 @@
 			</div>
 
 			<div class=" border-l-2">
-				<p class="px-3 pb-2 text-green-600 text-sm font-semibold">Action for Member States</p>
+				<p class="px-3 pb-2 text-sm font-semibold text-green-600">Action for Member States</p>
 				<ul class="flex flex-col gap-2">
 					{#each priorityArea?.attributes.memberStateActions || [] as action}
-						<li class="px-6 list-none border-l-2 hover:border-l-primary-600 -m-[2px]">
+						<li class="-m-[2px] list-none border-l-2 px-6 hover:border-l-primary-600">
 							<a
 								href="#{action.ActionTitle}"
-								class="hover:text-primary text-stone-900 text-sm transition-colors duration-300 ease-in-out"
+								class="text-sm text-stone-900 transition-colors duration-300 ease-in-out hover:text-primary"
 								>{action.ActionTitle}</a
 							>
 						</li>
@@ -179,12 +175,12 @@
 				</ul>
 			</div>
 
-			<div class=" pt-4 border-l-2">
+			<div class=" border-l-2 pt-4">
 				<ul class="flex flex-col gap-2">
-					<li class="px-3 list-none border-l-2 hover:border-l-primary-600 -m-[2px]">
+					<li class="-m-[2px] list-none border-l-2 px-3 hover:border-l-primary-600">
 						<a
 							href="#expected_outcome"
-							class="hover:text-primary text-sm transition-colors duration-300 ease-in-out"
+							class="text-sm transition-colors duration-300 ease-in-out hover:text-primary"
 							>Expected outcome</a
 						>
 					</li>
