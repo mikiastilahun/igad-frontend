04f76e6bdc3dca8f2168212946df907b7ab773f4
BIRUK TESFAYE <biruktesfayeve@gmail.com>
2024-06-12 13:18:20 +0300

    refactor: custom css properties and icons for share component

---
+++ src/lib/assets/icons/social/link.svg.svelte
@@ -0,0 +1,28 @@
+<script>
+	let className = '';
+	export { className as class };
+</script>
+
+<svg
+	class={className}
+	width="20"
+	height="20"
+	viewBox="0 0 20 20"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+	><path
+		id="Vector"
+		d="M11.1666 8.83301C12.8333 10.4997 12.8333 13.083 11.1666 14.7497L8.83329 17.083C7.16663 18.7497 4.58329 18.7497 2.91663 17.083C1.24996 15.4163 1.24996 12.833 2.91663 11.1663L4.99996 9.16634"
+		stroke="currentColor"
+		stroke-width="1.5"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	></path><path
+		id="Vector_2"
+		d="M8.83325 11.1665C7.16659 9.49984 7.16659 6.9165 8.83325 5.24984L11.1666 2.9165C12.8333 1.24984 15.4166 1.24984 17.0833 2.9165C18.7499 4.58317 18.7499 7.1665 17.0833 8.83317L14.9999 10.8332"
+		stroke="currentColor"
+		stroke-width="1.5"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	></path></svg
+>
---
+++ src/lib/assets/icons/social/x.svg.svelte
@@ -0,0 +1,23 @@
+<script>
+	let className = '';
+	export { className as class };
+</script>
+
+<svg
+	class={className}
+	xmlns="http://www.w3.org/2000/svg"
+	width="20"
+	height="20"
+	viewBox="0 0 20 20"
+	fill="none"
+	><path
+		d="M9.03492 12.6264L3.6718 18.7502H0.701172L7.64742 10.8152L9.03492 12.6264Z"
+		fill="currentColor"
+	></path><path
+		d="M10.6562 6.8675L15.5669 1.25H18.5356L12.0312 8.68875L10.6562 6.8675Z"
+		fill="currentColor"
+	></path><path
+		d="M19.6793 18.75H13.705L0.319336 1.25H6.44496L19.6793 18.75ZM14.5243 16.9731H16.1693L5.55121 2.93375H3.78621L14.5243 16.9731Z"
+		fill="currentColor"
+	></path></svg
+>
