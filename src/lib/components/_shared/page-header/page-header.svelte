8dfeec088c81920bc1d25cc078d841da370d5af5
BIRUK TESFAYE <biruktesfayeve@gmail.com>
2024-06-25 13:53:54 +0300

    refactor: Update page-header component to use flexbox for responsive layout

--- src/lib/components/_shared/page-header/page-header.svelte
+++ src/lib/components/_shared/page-header/page-header.svelte
@@ -9,9 +9,7 @@
 </script>
 
 <div class="relative md:p-4">
-	<section
-		class=" {className} relative flex min-h-[600px] items-center bg-primary-500 md:rounded-lg"
-	>
+	<section class=" {className} relative flex h-[600px] items-center bg-primary-500 md:rounded-lg">
 		{#if imgSrc}
 			<img
 				class="absolute inset-0 h-full w-full object-cover md:rounded-lg"
@@ -22,7 +20,9 @@
 				class="absolute inset-0 h-full w-full bg-gradient-to-r from-black/80 to-transparent md:rounded-lg"
 			></div>
 		{/if}
-		<div class="relative mx-auto w-full max-w-[1136px] px-4 md:px-8">
+		<div
+			class="relative mx-auto flex h-full w-full max-w-[1136px] flex-1 items-center px-4 md:px-8"
+		>
 			<div class="max-w-2xl text-white">
 				<h1 class="mb-2 text-3xl font-bold">{title}</h1>
 				{#if description && description.length > 0}
